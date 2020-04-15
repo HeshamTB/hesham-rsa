9f5d9fe5a5b8d84bc925f95deccc5f60f48da8ed
HeshamTB <hishaminv@gmail.com>
2020-04-16 00:05:03 +0300

    init commit

---
+++ rsa.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+#program to generate rsa key pair using methods in EE-305
+# Hesham Banafa 
+
+
+def main():
+    pass
+
+def getPrime(bits):
+    pass
+
+def isPrime(number):
+    pass
+
+
+
+if __name__ == "__main__":
+    main()
