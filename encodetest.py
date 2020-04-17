ad95ca82da7517a42b809994613d19c4ec9e4f57
HeshamTB <hishaminv@gmail.com>
2020-04-17 21:38:50 +0300

    Added key gen and encryption with d in test program.

--- encodetest.py
+++ encodetest.py
@@ -16,7 +16,7 @@ print(msg_list)
 
 import rsa as en
 
-key = en.readKeyFile('key')
+key = en.generateKeys()
 enc_list = list()
 pub_key = (key[0],key[1])
 for word in msg_list:
@@ -26,5 +26,14 @@ print(enc_list)
 unenc_list = list()
 
 for enc_word in enc_list:
-    unenc_list.append(en.decrypt(enc_word,key[2],key[0]).strip(('\x00')))
+    unenc_list.append(str(en.decrypt(enc_word,key[2],key[0])).strip(('\x00')))
 print(unenc_list)
+
+sig = "hesham"
+n = key[en.N]
+e = key[en.D] #encrypt with private key
+d = key[en.E]
+sig_enc = en.encrypt(sig,(n,e))
+print(sig_enc)
+sig_un = en.decrypt(sig_enc,d,n)
+print(sig_un)
